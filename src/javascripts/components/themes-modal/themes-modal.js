ef9dc8779d76b773a074878d023c24d84d22b04b
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-04-30 16:54:13 -0500

    WIP pushing partial form

--- src/javascripts/components/themes-modal/themes-modal.js
+++ src/javascripts/components/themes-modal/themes-modal.js
@@ -20,11 +20,58 @@ const modalDomStringBuilder = () => {
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
+
         <div class="modal-body">
         <div class="container-fluid">
+        <div class="row">
+        <div class="col-12">
           <p>Customize your dragon's chat experience with one of our gorgeous color schemes!</p>
           </div>
+          <form>
+  <div class="form-row">
+    <div class="form-group col-sm-12 col-md-6 col-lg-3">
+    <div class="form-check">
+  <input class="form-check-input themes-modal-radio" type="radio" name="themesRadios" id="fireRadio" value="fire">
+  <label class="form-check-label themes-radio-label" for="fireRadio">
+    Fire Theme
+  </label>
+</div>
+    </div>
+
+    <div class="form-group col-sm-12 col-md-6 col-lg-3">
+    <div class="form-check">
+  <input class="form-check-input themes-modal-radio" type="radio" name="themesRadios" id="iceRadio" value="ice">
+  <label class="form-check-label themes-radio-label" for="iceRadio">
+    Ice Theme
+  </label>
+</div>
+    </div>
+
+    <div class="form-group col-sm-12 col-md-6 col-lg-3">
+    <div class="form-check">
+  <input class="form-check-input themes-modal-radio" type="radio" name="themesRadios" id="stoneRadio" value="stone">
+  <label class="form-check-label themes-radio-label" for="stoneRadio">
+    Stone Theme
+  </label>
+</div>
+    </div>
+
+    <div class="form-group col-sm-12 col-md-6 col-lg-3">
+    <div class="form-check">
+  <input class="form-check-input themes-modal-radio" type="radio" name="themesRadios" id="originalRadio" value="original">
+  <label class="form-check-label themes-radio-label" for="originalRadio">
+    Reset to Original
+  </label>
+</div>
+    </div>
+
+          </div>
+          </form>
+
+          </div>
+          </div>
         </div>
+
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary modal-control-button" id="cancelButton" data-dismiss="modal">Cancel</button>
           <button type="button" class="btn btn-primary modal-control-button" id="saveButton">Save changes</button>
