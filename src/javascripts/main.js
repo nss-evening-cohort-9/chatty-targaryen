1428f0dd37a383956b2392d807100253ec171a53
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-04-30 20:50:14 -0500

    pushing working first stage of modal

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,12 @@
 import navbar from '../navbar';
 import menubar from '../menubar';
+import themesModal from './components/themes-modal/themes-modal';
 import '../styles/main.scss';
 
 const init = () => {
   navbar.buildNavBar();
   menubar.buildMenuBar();
+  themesModal.modalDomStringBuilder();
 };
 
 init();
