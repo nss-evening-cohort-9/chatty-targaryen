520013a4d784b860d1c02f9bc170b2a93817e6a4
Emily DeWitt <emilykdewitt@gmail.com>
2019-04-30 20:03:32 -0500

    Reformatted navbar and added menu bar with clear button and change themes button

--- src/menubar.js
+++ src/menubar.js
@@ -2,9 +2,10 @@ import util from './javascripts/helpers/util';
 
 const buildMenuBar = () => {
   let domString = '';
-  domString += '<div id="menuBar">';
-  domString += '<button class="btn btn-danger">Clear Messages</button>';
-  domString += '</div>';
+  domString += '<button id="clearBtn" class="btn btn-danger">Clear Messages</button>';
+  domString += '<button type="button" class="btn btn-primary modal-button" id="changeThemesButton" data-toggle="modal" data-target="#changeThemesModal">';
+  domString += 'Change Themes';
+  domString += '</button>';
   util.printToDom('menuBar', domString);
 };
 
